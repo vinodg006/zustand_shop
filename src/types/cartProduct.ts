96da6f7aeb72a24d3efffd910308af1cc0cc3f27
Vinod  Gupta <vinod.gupta@finicity.com>
2025-08-07 08:58:38 +0530

    Updates

--- src/types/cartProduct.ts
+++
@@ -1,3 +0,0 @@
-import type { Product } from "@/types/product";
-
-export type CartProduct = Product & { qty: number };
--- src/types/store.ts
+++
@@ -1,4 +0,0 @@
-import type { CartSlice } from "@/store/cart-slice";
-import type { UserSlice } from "@/store/user-slice";
-
-export type Store = UserSlice & CartSlice;
